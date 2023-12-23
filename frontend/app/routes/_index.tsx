2a6beb9749f7feadd1f0dad45378ab834c2892e1
2shiori17 <98276492+2shiori17@users.noreply.github.com>
2023-12-23 09:03:47 +0000

    feat(frontend): implement auth logic

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,41 +1,28 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunction } from "@remix-run/node";
+import { useLoaderData, Form } from "@remix-run/react";
+import { authenticator, User } from '~/services/auth.server';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await authenticator.isAuthenticated(request, {
+    failureRedirect: '/login',
+  });
+  return { user };
 };
 
 export default function Index() {
+  const data = useLoaderData<{ user: User }>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div>
+      {data.user && (
+        <>
+          <Form action="/logout" method="post">
+            <button>Logout</button>
+          </Form>
+          <h1>{data.user.id}</h1>
+        </>
+      )}
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
     </div>
   );
 }
