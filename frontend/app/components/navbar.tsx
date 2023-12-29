f192912a1b928acd5ad424407ffbc22d8e0b8cc3
2shiori17 <98276492+2shiori17@users.noreply.github.com>
2023-12-29 10:37:14 +0000

    feat(frontend): add notifications page

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -1,4 +1,5 @@
-import { BellIcon } from "@radix-ui/react-icons"
+import { Link } from "@remix-run/react";
+import { HomeIcon, BellIcon } from "@radix-ui/react-icons"
 
 import { Button } from "~/components/ui/button"
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar"
@@ -7,7 +8,14 @@ export function Navbar() {
   return (
     <nav className="flex flex-row justify-end items-center space-x-2 h-12 border-b px-2">
       <Button variant="ghost" size="icon">
-        <BellIcon className="h-6 w-6" />
+        <Link to="/app/groups">
+          <HomeIcon className="h-6 w-6" />
+        </Link>
+      </Button>
+      <Button asChild variant="ghost" size="icon">
+        <Link to="/app/notifications">
+          <BellIcon className="h-6 w-6" />
+        </Link>
       </Button>
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
