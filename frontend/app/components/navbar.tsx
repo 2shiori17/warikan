1999328ec9fbb2a9c74f69dc011282e07e3cd54e
2shiori17 <98276492+2shiori17@users.noreply.github.com>
2023-12-29 12:24:34 +0000

    feat(frontend): add settings page

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -7,7 +7,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar"
 export function Navbar() {
   return (
     <nav className="flex flex-row justify-end items-center space-x-2 h-12 border-b px-2">
-      <Button variant="ghost" size="icon">
+      <Button asChild variant="ghost" size="icon">
         <Link to="/app/groups">
           <HomeIcon className="h-6 w-6" />
         </Link>
@@ -17,10 +17,12 @@ export function Navbar() {
           <BellIcon className="h-6 w-6" />
         </Link>
       </Button>
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
+      <Link to="/app/settings">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </Link>
     </nav>
   )
 }
